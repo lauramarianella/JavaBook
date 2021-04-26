b927765d9448854d62784e434c0195bf57cb7d19
lauramarianella <lauramarianella@gmail.com>
2021-04-26 16:31:03 -0400

    Collections

---
+++ src/main/java/MyPkg/CH_23/Sorting/_23_04_BubbleSort.java
@@ -0,0 +1,37 @@
+package MyPkg.CH_23.Sorting;
+
+public class _23_04_BubbleSort {
+    public static int numSwaps=0;
+    /** Bubble sort method */
+    public static void bubbleSort(int[] list) {
+      boolean needNextPass = true;
+
+      for (int k = 1; k < list.length && needNextPass; k++) {
+        // Array may be sorted and next pass not needed
+        needNextPass = false;
+        for (int i = 0; i < list.length - k; i++) {
+          if (list[i] > list[i + 1]) {
+            // Swap list[i] with list[i + 1]
+            int temp = list[i];
+            list[i] = list[i + 1];
+            list[i + 1] = temp;
+
+            needNextPass = true; // Next pass still needed
+            numSwaps++;
+          }
+        }
+      }
+  }
+
+  /** A test method */
+  /*int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};*/
+  public static void main(String[] args) {
+    
+    int[] list={9,8,6,5,4,2,1};//n=7 = > 21 swaps
+    bubbleSort(list);
+    for (int i = 0; i < list.length; i++)
+      System.out.print(list[i] + " ");
+    
+    System.out.println("\n"+ numSwaps);
+  }
+}
